4ace20208f17de427140b7b7edb95dee46de8596
GanJL <jianlin.gan.2020@scis.smu.edu.sg>
2022-01-16 20:22:49 +0800

    removed msg debugging

--- client/src/screens/Login/Login.js
+++ client/src/screens/Login/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
 
                 <a href='' className="reRouteLogin" onClick={reRouteRegister}>Need an account? Click to register!</a>
 
-                {error && <div className='registerError'>{error[0].msg}</div>}
+                {/* {error && <div className='registerError'>{error[0] ? error[0].msg : ""}</div>} */}
 
                 <div className='container text-center'>
                     <button onClick = {handleSubmit} className="mt-3 newbtn">Login</button>
