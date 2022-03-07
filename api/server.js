af37410c35b9a475b01f5fa2cfce7e55cfd79245
GanJL <jianlin.gan.2020@scis.smu.edu.sg>
2022-03-07 21:34:05 +0800

    redeploy

--- api/server.js
+++ api/server.js
@@ -34,19 +34,19 @@ app.use('/api/tasks', taskRoutes);
 
 // serve static client with backend server
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 5000; 
